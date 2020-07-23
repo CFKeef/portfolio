adf9cb3fa3d4a9286fdb4e0bcf9e8a0129889bb2
Patryck <patryckg@outlook.com>
2020-07-22 20:58:05 -0400

    Bio Completed on Mobile
    The section covering name and major is done for mobile!

--- src/Components/Bio/Bio.js
+++ src/Components/Bio/Bio.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import SectionHeader from '../../Assets/Section1.svg';
 import './Bio.css';
 
 const Bio = () => {
     return (
-        <p>Test</p>
+        <div className="Bio-Container">
+            <h1>Hi, I'm <mark className="pink-highlight">Patryck Golebiewski</mark></h1>
+            <h2>Computer Science and Accounting student based in <mark className="purple-highlight">NYC</mark></h2>
+            <img src={SectionHeader} alt="Check out work below"></img>
+        </div>
     )
 }
 
