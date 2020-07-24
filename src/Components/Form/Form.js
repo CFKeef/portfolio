a3131aad43bdbc543a7c76c4fc57689546360642
CFKeef <Patryck.Golebiewski29@qmail.cuny.edu>
2020-07-24 15:41:45 -0400

    Mobile: Form 50%
    Form is structured and styled for mobile.
    
    Need to set up express to handle the post request

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -5,8 +5,26 @@ import './Form.css';
 class Form extends React.Component {
     render() {
         return(
-            <div>
-                
+            <div className="form-container">
+                <form action='/contacted' id='contact-form' method='POST' role='form'>
+                    <fieldset>
+                        <div>
+                            <label for="name"></label>
+                            <input id="name" name="name" type="text" placeholder="Name"></input>
+                        </div>
+                        <div>
+                            <label for="email"></label>
+                            <input id="email" name="email" type="email" placeholder="Email Address"></input>
+                        </div>
+                        <div>
+                            <label for="body"></label>
+                            <textarea id="body" name="body" type="text" placeholder="Message"></textarea>
+                        </div>
+                        <div className="button-container">
+                            <button type="submit">Submit</button>
+                        </div>
+                    </fieldset>
+                </form>
             </div>
         )
     }
