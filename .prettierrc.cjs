6171ece93bb6b0e4f0c9406f6c98b25871bcfef0
CFKeef <Patryckg@outlook.com>
2023-08-13 12:12:33 -0400

    base

---
+++ .prettierrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  pluginSearchDirs: false,
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  astroAllowShorthand: false,
+  tailwindConfig: "./tailwind.config.cjs",
+  arrowParens: "always",
+  printWidth: 80,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: "all",
+  tabWidth: 2,
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+import image from "@astrojs/image";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), solidJs(), image(), mdx()]
+});
