6fee1f0124a2ef8511d9e588e0a4936a451824de
CFKeef <Patryckg@outlook.com>
2024-04-03 16:05:42 -0400

    fix: correct target and relation tags on anchors

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
 function Footer() {
   return (
     <footer class="flex gap-4">
-      <a href="https://github.com/CFKeef">
-        <div class="i-lucide:github text-white hover:text-yellow  h-6 w-6" title="Github" />
+      <a href="https://github.com/CFKeef" target="_blank" rel="noreferrer">
+        <div class="i-lucide:github text-white hover:text-brand-red  h-6 w-6" title="Github" />
       </a>
-      <a href="https://www.linkedin.com/in/patryckg/">
-        <div class="i-lucide:linkedin text-white hover:text-yellow h-6 w-6" title="Linkedin" />
+      <a href="https://www.linkedin.com/in/patryckg/" target="_blank" rel="noreferrer">
+        <div class="i-lucide:linkedin text-white hover:text-brand-red h-6 w-6" title="Linkedin" />
       </a>
     </footer>
   )
---
+++ src/components/Projects.tsx
@@ -0,0 +1,36 @@
+import { For } from 'solid-js'
+
+type Project = {
+  name: string
+  description: string
+  link: string
+}
+
+const projects = [
+  {
+    name: 'keef.sh',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    link: 'https://github.com/CFKeef/portfolio/',
+  },
+]
+
+export const Projects = () => {
+  return (
+    <div class="grid grid-cols-3 gap-2">
+      <For each={projects}>{(e) => <Project {...e} />}</For>
+    </div>
+  )
+}
+
+const Project = (props: Project) => {
+  return (
+    <a
+      href={props.link}
+      class="w-full bg-foreground p-2 rounded hover:bg-brand-red/80"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <p>{props.name}</p>
+    </a>
+  )
+}
