f2ed937039e8275d1462e9a4778f33729134567b
Patryck <PatryckG@outlook.com>
2024-05-22 16:58:58 -0400

    fix: redis move to just construct on call

--- src/server/cache.ts
+++ src/server/cache.ts
@@ -1,10 +1,9 @@
 import { Redis } from '@upstash/redis'
 
-const cache = new Redis({
-  url: process.env.CACHE_URL as string,
-  token: process.env.CACHE_PASSWORD as string,
-})
-
 export const getRedis = async () => {
-  return cache
+  return new Redis({
+    url: process.env.CACHE_URL as string,
+    token: process.env.CACHE_PASSWORD as string,
+  })
+  
 }
