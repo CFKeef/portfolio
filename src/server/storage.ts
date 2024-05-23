119f61d8ec7ba312cd161df8dec3d07685ad08dd
Patryck <PatryckG@outlook.com>
2024-05-23 17:42:58 -0400

    might be related to ${}

--- src/server/storage.ts
+++ src/server/storage.ts
@@ -7,7 +7,7 @@ const bucketName = "portfolio";
 
 const storage = new S3Client({
     region: "auto",
-    endpoint: `https://${process.env.CF_ACCOUNT_ID as string}.r2.cloudflarestorage.com`,
+    endpoint: "https://" + process.env.CF_ACCOUNT_ID as string + ".r2.cloudflarestorage.com",
     credentials: {
       accessKeyId: process.env.CF_ACCESS_ID as string,
       secretAccessKey:  process.env.CF_ACCESS_SECRET as string,
